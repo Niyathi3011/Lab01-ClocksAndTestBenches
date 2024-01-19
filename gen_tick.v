2f7b29ffdf230d0d550e762c96cb6507b84a0678
Niyathi Allu <59156086+Niyathi3011@users.noreply.github.com>
2024-01-19 21:16:50 +0000

    Finished

--- gen_tick.v
+++ gen_tick.v
@@ -20,12 +20,22 @@ module gen_tick # ( parameter SRC_FREQ = 5000, parameter TICK_FREQ = 1) (
 );
 
 // Declare registers and wires here
+reg[31:0] limit = SRC_FREQ/TICK_FREQ /2 -1;
+reg[31:0] acc = 0;
+reg tick_out = 0;
 
 always @(posedge src_clk) begin
     // put your code for the multiplier here
+    if(enable) begin
+        acc <= acc+1;
+        if(acc == limit) begin
+            acc <= 0;
+            tick_out <= ~tick_out;
+        end
+    end
 end
 
 // Change this assign statement to the actual tick value
-assign tick = src_clk;
+assign tick = tick_out;
 
 endmodule
